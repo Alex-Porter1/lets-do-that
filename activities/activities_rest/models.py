24369cf5a1e60aee2bf1856ece4d8d546878d7ab
Alex Porter <alpo1992@gmail.com.here>
2022-08-22 16:50:39 -0600

    created models for activities, rating and category

--- activities/activities_rest/models.py
+++ activities/activities_rest/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Rating(models.Model):
+    value = models.SmallIntegerField()
+    description = models.TextField()
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    picture_url = models.CharField(max_length=256)
+    category = models.ForeignKey(
+        Category,
+        related_name = "categories",
+        on_delete=models.PROTECT
+    )
+    rating = models.ForeignKey(
+        Rating,
+        related_name = "ratings",
+        on_delete=models.PROTECT
+    )
