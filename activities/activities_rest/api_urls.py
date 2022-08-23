edd41e2066753b015af695ede3ab9c375eea8450
Alex Porter <alpo1992@gmail.com.here>
2022-08-23 15:20:01 -0600

    added views to create, update, view detail category

--- activities/activities_rest/api_urls.py
+++ activities/activities_rest/api_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import api_list_activities, api_list_categories, api_list_ratings, api_show_activities
+from .views import api_list_activities, api_list_categories, api_list_ratings, api_show_activities, api_show_categories
 
 urlpatterns = [
     path("activities/", api_list_activities, name="api_list_activities"),
     path("categories/", api_list_categories, name="api_list_categories"),
+    path("categories/<int:pk>/", api_show_categories, name="api_show_categories"),
     path("ratings/", api_list_ratings, name="api_list_ratings"),
     path("activities/<int:pk>/", api_show_activities, name="api_show_activities")
 ]
--- activities/activities_rest/views.py
+++ activities/activities_rest/views.py
@@ -74,19 +74,29 @@ def api_list_activities(request):
                 status=400,
             )
 
-@require_http_methods(["GET"])
+@require_http_methods(["GET", "POST"])
 def api_list_categories(request):
     if request.method == "GET":
-        categories = Category.objects.all()
+        categories = Category.objects.all().order_by("name")
         return JsonResponse(
             {"categories": categories},
-            encoder=CategoryEncoder,
+            encoder=CategoryEncoder
         )
     else:
-        return JsonResponse(
-            {"message": "Category doesn't exist"},
-            status=400
-        )
+        content = json.loads(request.body)
+        try:
+            category = Category.objects.create(**content)
+            return JsonResponse(
+                category,
+                encoder=CategoryEncoder,
+                safe=False,
+            )
+        except IntegrityError:
+            return JsonResponse(
+                {"message": "Category ID already exists"},
+                status=400,
+            )
+
 
 @require_http_methods(["GET"])
 def api_list_ratings(request):
@@ -124,3 +134,63 @@ def api_show_activities(request, pk):
     else:
         count, _ = Activity.objects.filter(id=pk).delete()
         return JsonResponse({"deleted": count > 0})
+
+@require_http_methods(["GET", "POST"])
+def api_list_activities(request):
+    if request.method == "GET":
+        activities = Activity.objects.all().order_by("name")
+        return JsonResponse(
+            {"activities": activities},
+            encoder=ActivityListEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            category = Category.objects.get(id=content["category"])
+            content["category"] = category
+        except Category.DoesNotExist:
+            return JsonResponse(
+                {"message": "Category does not exist"},
+                status=400
+            )
+        if content["rating"]:
+            rating = Rating.objects.get(id=content["rating"])
+            content["rating"] = rating
+        else:
+            content["rating"] = "No rating given"
+        try:
+            activity = Activity.objects.create(**content)
+            return JsonResponse(
+                activity,
+                encoder=ActivityListEncoder,
+                safe=False,
+            )
+        except IntegrityError:
+            return JsonResponse(
+                {"message": "Activity ID already exists"},
+                status=400,
+            )
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_show_categories(request, pk):
+    if request.method == "GET":
+        category = Category.objects.get(id=pk)
+        return JsonResponse(
+            category,
+            encoder=CategoryEncoder,
+            safe=False,
+        )
+    elif request.method == "PUT":
+        content = json.loads(request.body)
+        Category.objects.filter(id=pk).update(**content)
+        category = Category.objects.get(id=pk)
+        return JsonResponse(
+            category,
+            encoder=CategoryEncoder,
+            safe=False,
+        )
+    else:
+        count, _ = Category.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+
