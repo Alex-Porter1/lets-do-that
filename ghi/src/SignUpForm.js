ef71f575d96fdf723799197024054a4926780195
perlamcarlson <perlamcarlson@gmail.com>
2022-08-24 22:51:29 -0700

    playing

--- ghi/src/SignUpForm.js
+++ ghi/src/SignUpForm.js
@@ -1,10 +1,53 @@
-function SignUpForm() {
-<div>
-    
-<a> Sign Up here </a>
-   </div>
+import React from "react";
 
-}
+import {
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Button
+} from "reactstrap";
 
+const SignUpForm = () => {
+  return (
+    <form>
+      <FormGroup>
+        <Label for="exampleEmail">Email address</Label>
+        <Input
+          type="email"
+          name="email"
+          id="exampleEmail"
+          placeholder="Enter email"
+        />
+        <FormText color="muted">
+          We'll never share your email with anyone else.
+        </FormText>
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          type="password"
+          name="password"
+          id="examplePassword"
+          placeholder="Password"
+          autoComplete="off"
+        />
+      </FormGroup>
+      <FormGroup check>
+        <Label check>
+          <Input type="checkbox" />{' '}
+          Check me out
+          <span className="form-check-sign">
+            <span className="check"></span>
+        </span>
+        </Label>
+      </FormGroup>
+      <Button color="primary" type="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
 
 export default SignUpForm;
+
