4762058cdfd55ff0b5142f268de3e526634c0d1c
perlamcarlson <perlamcarlson@gmail.com>
2022-08-24 17:53:43 -0700

    going back

---
+++ ghi/src/Nav.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+    <div class="collapse navbar-collapse justify-content-end" id="navigation">
+    <ul class="navbar-nav">
+    <NavLink className="nav-link" aria-current="page" to="/signup/new">Sign Up</NavLink>
+            
+      <li class="nav-item">
+    </li>
+    </ul>
+    </div>
+
+
+}
+
+
+export default Nav;
