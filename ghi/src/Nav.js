a5534a010f43ee50e224e4ef8be771aa62d11b2c
ljordan <lmjordan104@gmail.com>
2022-08-31 15:07:16 -0700

    slight change

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/activities">Lets Do That</NavLink>
+        <NavLink className="navbar-brand" to="/">Lets Do That</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
