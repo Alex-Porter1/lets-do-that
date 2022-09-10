5fe9f582286b88913ed42cf502eccf057141d8b7
Alex Porter <alpo1992@gmail.com.here>
2022-09-10 11:12:37 -0600

    more linting changes

--- activities/tests/test_01.py
+++ activities/tests/test_01.py
@@ -68,8 +68,8 @@ class FeatureTests(TestCase):
             self.assertEqual(
                 category.field.related_query_name(),
                 "categories",
-                msg=
-                ("Activity.category should have a related name of 'categories'"),
+                msg=\
+                "Activity.category should have a related name 'categories'",
             )
         except ModuleNotFoundError:
             self.fail("Could not find 'Activity.models'")
