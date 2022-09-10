daba4976f4217e6bc6e626cbbf42b49f01755774
Alex Porter <alpo1992@gmail.com.here>
2022-09-10 17:30:35 -0600

    testing workaround for unused variable

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -16,7 +16,7 @@ function BootstrapInputFields(props) {
 function LoginForm(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [token_, login] = useToken();
+  const [token_, login] = useToken();  // eslint-disable-line no-use-before-define
   const navigate = useNavigate();
   const { token } = useAuthContext();
   const signupPhrase = `Don't have an account? \n Sign up here`;
