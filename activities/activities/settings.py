16d46bb19620e95dc05a86a53d662c0c29e75319
Joel <jcswansontamu@gmail.com>
2022-09-12 15:30:25 -0700

    Updating deployment files, commeted out tests

--- activities/activities/settings.py
+++ activities/activities/settings.py
@@ -16,7 +16,6 @@ import os
 
 import dj_database_url
 
-from accounts.accounts.settings import ALLOWED_HOSTS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
