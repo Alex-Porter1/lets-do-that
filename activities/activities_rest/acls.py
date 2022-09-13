824fe5bf09ef2c617e58f26f98cb0273af24d075
Joel <jcswansontamu@gmail.com>
2022-09-13 14:44:04 -0700

    Updating to yelp api from github

--- activities/activities_rest/acls.py
+++ activities/activities_rest/acls.py
@@ -1,31 +1,36 @@
 import os
 import json
 import requests
-# from requests_oauthlib import OAuth1
-# from yelp.client import Client
+from yelpapi import YelpAPI
 
 YELP_API_KEY = os.environ["YELP_API_KEY"]
 YELP_URL = os.environ["YELP_URL"]
 # cors = "https://thingproxy.freeboard.io/fetch/"
-cors = "https://cors-anywhere.herokuapp.com/"
+# cors = "https://cors-anywhere.herokuapp.com/"
 
 
 # client = Client(YELP_API_KEY)
 
 def get_yelp_list (category, location):
-    url = f"{YELP_URL}search?location=${location}&categories=${category}"
-    headers = {
-        "Authorization": f"Bearer {YELP_API_KEY}",
-        "accept": "application/json",
-        "x-requested-with": "xmlhttprequest",
-        "Access-Control-Allow-Origin": "*",
-    }
-    final_url = f"{cors}{url}"
-    # final_url = url
-    response = requests.get(final_url, headers=headers)
-    content = json.loads(response.content)
-    print("final url", final_url)
+    yelp_api = YelpAPI(YELP_API_KEY)
+    response = yelp_api.search_query(category=category, location=location)
+    # print("response", response)
+    # content = json.loads(response.content)
+    # print("content:", content)
 
-    return content
+    return response
+
+# def get_yelp_list (category, location):
+#     url = f"{YELP_URL}search?location=${location}&categories=${category}"
+#     headers = {
+#         "Authorization": f"Bearer {YELP_API_KEY}",
+#     }
+#     final_url = f"{cors}{url}"
+#     # final_url = url
+#     response = requests.get(final_url, headers=headers)
+#     content = json.loads(response.content)
+#     print("final url", final_url)
+
+#     return content
 
 
