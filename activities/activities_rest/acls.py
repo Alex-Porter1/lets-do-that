1f618e6b7c1b9c7cf43a729e8cae2a67a0ebc899
Joel <jcswansontamu@gmail.com>
2022-09-13 11:58:29 -0700

    Updated to new key

--- activities/activities_rest/acls.py
+++ activities/activities_rest/acls.py
@@ -2,22 +2,14 @@ import os
 import json
 import requests
 from requests_oauthlib import OAuth1
+from yelp.client import Client
 
 YELP_API_KEY = os.environ["YELP_API_KEY"]
-
-ICON_API_KEY = os.environ["ICON_API_KEY"]
-ICON_SECRET = os.environ["ICON_SECRET"]
+YELP_URL = os.environ["YELP_URL"]
 
 
-def get_icon(term):
+def get_yelp_list (category, location):
+    pass
 
-    auth = OAuth1(ICON_API_KEY, ICON_SECRET)
-    endpoint = f'http://api.thenounproject.com/icons/{term}'
 
-    response = requests.get(endpoint, auth=auth)
-    print(response.content)
-    content = json.loads(response.content)
 
-    icon = content["icons"][0]["preview_url"]
-
-    return icon
