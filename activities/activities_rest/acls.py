b26605f79bc674f10a178c3e46a75aeb6332c37a
Joel <jcswansontamu@gmail.com>
2022-09-13 14:22:35 -0700

    Testing CORS

--- activities/activities_rest/acls.py
+++ activities/activities_rest/acls.py
@@ -6,7 +6,8 @@ import requests
 
 YELP_API_KEY = os.environ["YELP_API_KEY"]
 YELP_URL = os.environ["YELP_URL"]
-cors = "https://thingproxy.freeboard.io/fetch/"
+# cors = "https://thingproxy.freeboard.io/fetch/"
+cors = "https://cors-anywhere.herokuapp.com/"
 
 
 # client = Client(YELP_API_KEY)
@@ -15,11 +16,15 @@ def get_yelp_list (category, location):
     url = f"{YELP_URL}search?location=${location}&categories=${category}"
     headers = {
         "Authorization": f"Bearer {YELP_API_KEY}",
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
     }
     final_url = f"{cors}{url}"
     # final_url = url
     response = requests.get(final_url, headers=headers)
     content = json.loads(response.content)
+    print("final url", final_url)
 
     return content
 
